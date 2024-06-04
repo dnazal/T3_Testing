126844388751612f4b2ced88256fab8662ecf292
Diego Nazal <diegonazal@Diegos-MacBook-Air.local>
2024-06-04 11:29:22 -0400

    cherry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,10 +11,15 @@ require 'capybara/rspec'
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("start-maximized")
+  options.add_argument("disable-infobars")
+  options.add_argument("--disable-extensions")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--no-sandbox")
+  if headless:
   options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--window-size=1280,960')
+  options.binary_location = "/usr/bin/chromium-browser"
+  self.driver = webdriver.Chrome(options=options)
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
