81d12ab90fd18e8b1f43b9b5bee29b1962bbb180
Diego Nazal <diegonazal@Diegos-MacBook-Air.local>
2024-06-04 16:24:48 -0400

    Partiendo de nuevo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,20 +8,20 @@ SimpleCov.start 'rails' do
 end
 
 require 'capybara/rspec'
+require 'selenium-webdriver'
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("start-maximized")
-  options.add_argument("disable-infobars")
-  options.add_argument("--disable-extensions")
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--no-sandbox")
-  options.add_argument('--headless')
-  options.add_argument('--remote-debugging-port=9222')
-  options.binary_location = "/usr/bin/chromedriver"
-  self.driver = webdriver.Chrome(options=options)
+  options.add_argument('--headless') # Run in headless mode
+  options.add_argument('window-size=1920,1080') # Specify window size
+  options.add_argument('disable-gpu') # This flag is often recommended to avoid hardware acceleration issues
+  options.add_argument('no-sandbox') # Bypass OS security model, REQUIRED on TravisCI and other CI platforms
+  options.add_argument('disable-dev-shm-usage') # Overcome limited resource problems
+  options.add_argument('disable-infobars') # Disabling infobars helps in automation by disabling popups
+  options.add_argument('--disable-extensions') # Disable extensions
+  options.add_argument('--remote-debugging-port=9222') # Remote debugging port
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :headless_chrome
